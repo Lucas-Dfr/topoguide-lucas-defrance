ec423f411650824de716cfc2190c0a6eb4e192d9
Lucas Defrance <lucas.defrance@student.isae-supaero.fr>
2022-04-17 12:22:49 +0200

    Create model for itineraries

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Itinineraire(models.Model):
+    
+    CHOIX_DIFFICULTE = [
+        (1,'1 - Très facile'),
+        (2,'2 - Facile'),
+        (3,'3 - Modérée'),
+        (4,'4 - Difficile'),
+        (5, '5 - Très difficile'),
+    ]
+    
+    nom = models.CharField('Nom',max_length=200)
+    p_dep = models.CharField('Point de départ',max_length=200)
+    description = models.CharField('Description de la sortie',max_length=3000)
+    alt_dep = models.IntegerField('Altitude de départ')
+    altitude_mim = models.IntegerField('Altitude minimale')
+    altitude_max = models.IntegerField('Altitude maximale')
+    denivele_pos = models.IntegerField('Denivelé positif cumulé')
+    denivele_neg = models.IntegerField('Denivelé negatif cumulé')
+    duree = models.TimeField('Durée estimée',auto_now=False, auto_now_add=False)
+    difficulte = models.IntegerField('Difficulté estimée', choices = CHOIX_DIFFICULTE)
+    
+    
