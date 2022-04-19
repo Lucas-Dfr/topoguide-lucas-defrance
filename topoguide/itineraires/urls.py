071053d8a8b4718730c4f27d3291fdfda8630156
Lucas Defrance <lucas.defrance@student.isae-supaero.fr>
2022-04-19 18:01:34 +0200

    Better name for the path

--- topoguide/itineraires/urls.py
+++ topoguide/itineraires/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('',views.liste_itineraires, name = 'Liste des itineraires'),
-    path('sorties/<int:itineraire_id>/', views.liste_sorties, name='Liste des sorties'),
-    path('sortie/<int:sortie_id>/', views.detail_sortie, name='Détail de la sortie'),
+    path('sorties/<int:itineraire_id>/', views.liste_sorties, name='sorties_liste'),
+    path('sortie/<int:sortie_id>/', views.detail_sortie, name='sortie_detail'),
+    path('nouvelle_sortie/', views.nouvelle_sortie, name = "sortie_new")
     
 ]
 
