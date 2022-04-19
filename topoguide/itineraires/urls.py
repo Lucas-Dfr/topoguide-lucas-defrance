e054a3dd1dfa032736a7c7cce8a2f560ee2b92e3
Lucas Defrance <lucas.defrance@student.isae-supaero.fr>
2022-04-19 20:19:01 +0200

    Form to add a new excursion

--- topoguide/itineraires/urls.py
+++ topoguide/itineraires/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'itineraires'
 urlpatterns = [
-    path('',views.liste_itineraires, name = 'Liste des itineraires'),
+    path('',views.liste_itineraires, name = 'itineraires_liste'),
     path('sorties/<int:itineraire_id>/', views.liste_sorties, name='sorties_liste'),
     path('sortie/<int:sortie_id>/', views.detail_sortie, name='sortie_detail'),
     path('nouvelle_sortie/', views.nouvelle_sortie, name = "sortie_new")
