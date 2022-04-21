86a8e0a377a057b4c05698de6010eb5334bdcd17
Lucas Defrance <lucas.defrance@student.isae-supaero.fr>
2022-04-21 09:43:50 +0200

    Remove useless use of django.translate

--- topoguide/itineraires/forms.py
+++ topoguide/itineraires/forms.py
@@ -1,6 +1,5 @@
 from django.forms import ModelForm
 from itineraires.models import Sortie, Itineraire
-from django.utils.translation import gettext_lazy as _
 from django import forms
 
 class ExcursionForm(ModelForm):
@@ -8,13 +7,13 @@ class ExcursionForm(ModelForm):
         model = Sortie
         fields = ['itineraire', 'date', 'duree_reelle', 'nb_personnes', 'experience', 'meteo', 'difficulte']
         labels = {
-            'itineraire': _('Itinéraire'),
-            'date': _('Date de la sortie'),
-            'duree_reelle': _('Durée réelle de la sortie'),
-            'nb_personnes': _('Nombre de personnes'),
-            'experience': _('Expérience du groupe'),
-            'meteo': _('Météo'),
-            'Difficulte': _('Difficultée ressentie'),
+            'itineraire': 'Itinéraire',
+            'date': 'Date de la sortie',
+            'duree_reelle': 'Durée réelle de la sortie',
+            'nb_personnes': 'Nombre de personnes',
+            'experience': 'Expérience du groupe',
+            'meteo': 'Météo',
+            'Difficulte': 'Difficultée ressentie',
         }
         widgets = {
         'date': forms.DateInput(format=('%m/%d/%Y'), attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),
