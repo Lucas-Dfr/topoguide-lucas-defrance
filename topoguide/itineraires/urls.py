4df2b8661e768d112af268b9a3ad8375e3df7077
Lucas Defrance <lucas.defrance@student.isae-supaero.fr>
2022-04-21 10:10:26 +0200

    Preparing the form to edit an excursion

--- topoguide/itineraires/urls.py
+++ topoguide/itineraires/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('',views.liste_itineraires, name = 'itineraires_liste'),
     path('sorties/<int:itineraire_id>/', views.liste_sorties, name='sorties_liste'),
     path('sortie/<int:sortie_id>/', views.detail_sortie, name='sortie_detail'),
-    path('nouvelle_sortie/', views.nouvelle_sortie, name = "sortie_new")
+    path('nouvelle_sortie/', views.nouvelle_sortie, name = "sortie_new"),
+    #path('modif_sortie/<int:sortie_id>/', views.modif_sortie, name = "sortie_update"),
     
 ]
 
