24fd52ce1b70ff100944f0b930ab542e98e84c1b
Lucas Defrance <lucas.defrance@student.isae-supaero.fr>
2022-04-22 09:45:09 +0200

    Comment and indent

--- topoguide/itineraires/admin.py
+++ topoguide/itineraires/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Itineraire, Sortie
 
 class ItineraireAdmin(admin.ModelAdmin):
+    # Register itinerary models and order them in coherent fields
     fieldsets = [
         ('Description générale', {'fields': ('nom','duree','difficulte','description')}),
         ('Informations complémentaires', {'fields': ('p_dep','alt_dep','altitude_min','altitude_max','denivele_pos','denivele_neg')}),
@@ -11,6 +12,7 @@ class ItineraireAdmin(admin.ModelAdmin):
 admin.site.register(Itineraire, ItineraireAdmin)
 
 class SortieAdmin(admin.ModelAdmin):
+    # Register excursion models and diplay only the key information
     list_display = ('utilisateur','itineraire', 'date')
 
 admin.site.register(Sortie, SortieAdmin)
--- topoguide/itineraires/forms.py
+++ topoguide/itineraires/forms.py
@@ -3,6 +3,9 @@ from itineraires.models import Sortie, Itineraire
 from django import forms
 
 class NewSortieForm(ModelForm):
+    """
+    Form made to create a new excursion
+    """
     class Meta:
         model = Sortie
         fields = ['itineraire', 'date', 'duree_reelle', 'nb_personnes', 'experience', 'meteo', 'difficulte']
@@ -20,7 +23,10 @@ class NewSortieForm(ModelForm):
     }
 
 class UpdateSortieForm(ModelForm):
-    # A user cannot change the itinerary corresponding to his excursion so the field "itinerary" has been removed
+    """
+    Same form as above but the field "itinerary" has been removed since a user 
+    cannot change the itinerary corresponding to his excursion
+    """
     class Meta:
         model = Sortie
         fields = ['date', 'duree_reelle', 'nb_personnes', 'experience', 'meteo', 'difficulte']
