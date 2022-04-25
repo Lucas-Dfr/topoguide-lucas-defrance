71d63ad4bc2c61bac1b7acea2992b27f1cc0d823
Lucas Defrance <lucas.defrance@student.isae-supaero.fr>
2022-04-25 14:51:08 +0200

    Again change of name in venv repository

--- topoguide/topoguide/settings.py
+++ topoguide/topoguide/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_bootstrap5',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
